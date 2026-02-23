2d0575e8eff7c5154dc252cb61c57f105adad0e5
deepesh224-ux <deepesh2005august@gmail.com>
2026-02-23 21:54:18 +0530

    app.py: Billing, contract, and raw input construction (Lines 61-90)

--- app.py
+++ app.py
@@ -58,3 +58,33 @@ else:
         st.subheader("Contract & Billing")
         col4, col5, col6 = st.columns(3)
         
+        with col4:
+            streaming_tv = st.selectbox("Streaming TV", ["No", "Yes", "No internet service"])
+            streaming_movies = st.selectbox("Streaming Movies", ["No", "Yes", "No internet service"])
+
+        with col5:
+            contract = st.selectbox("Contract", ["Month-to-month", "One year", "Two year"])
+            paperless_billing = st.selectbox("Paperless Billing", ["Yes", "No"])
+
+        with col6:
+            payment_method = st.selectbox("Payment Method", [
+                "Electronic check", "Mailed check", "Bank transfer (automatic)", "Credit card (automatic)"
+            ])
+            monthly_charges = st.number_input("Monthly Charges ($)", min_value=0.0, value=50.0)
+            total_charges = st.number_input("Total Charges ($)", min_value=0.0, value=50.0)
+
+        submit = st.form_submit_button("Predict Churn Risk")
+
+    if submit:
+
+        raw_input = {
+            'gender': gender,
+            'SeniorCitizen': senior_citizen,
+            'Partner': partner,
+            'Dependents': dependents,
+            'tenure': tenure,
+            'PhoneService': phone_service,
+            'MultipleLines': multiple_lines,
+            'InternetService': internet_service,
+            'OnlineSecurity': online_security,
+            'OnlineBackup': online_backup,
